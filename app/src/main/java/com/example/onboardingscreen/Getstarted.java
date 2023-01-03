261fd0c47d0cfa0d47be13aa5eb3cfe4a2b4c7fa
ms <mayurms5060@gmail.com>
2023-01-03 17:03:31 +0530

    upload on github

---
+++ app/src/main/java/com/example/onboardingscreen/Getstarted.java
@@ -0,0 +1,14 @@
+package com.example.onboardingscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Getstarted extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_getstarted);
+    }
+}
